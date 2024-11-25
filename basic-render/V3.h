1de06ac0bdbbf429f9150b5921fbf4899c116401
Oleksii Tkachuk <tkachuk.alexey@unicyb.kiev.ua>
2024-11-25 02:42:34 +0200

    V2 and V3 classes are now inline

--- basic-render/V3.h
+++ basic-render/V3.h
@@ -27,14 +27,43 @@ union V3
 
 	f32 e[3];
 
-	V3();
-	V3(f32 X, f32 Y, f32 Z);
+	inline V3();
+	inline V3(f32 X, f32 Y, f32 Z);
 
-	V3 operator+(const V3& other) const;
-	V3 operator*(f32 scalar) const;
+	inline V3 operator+(const V3& other) const;
+	inline V3 operator*(f32 scalar) const;
 
-	friend V3 operator*(f32 scalar, const V3& v3);
+	inline friend V3 operator*(f32 scalar, const V3& v3);
 
-	V2 getXY() const;
-	V2 getYZ() const;
+	inline V2 getXY() const;
+	inline V2 getYZ() const;
 };
+
+V3::V3() : x(0), y(0), z(0) {}
+
+V3::V3(f32 X, f32 Y, f32 Z) : x(X), y(Y), z(Z) {}
+
+V3 V3::operator+(const V3& other) const
+{
+	return V3(x + other.x, y + other.y, z + other.z);
+}
+
+V2 V3::getXY() const
+{
+	return V2(x, y);
+}
+
+V2 V3::getYZ() const
+{
+	return V2(y, z);
+}
+
+V3 V3::operator*(f32 scalar) const
+{
+	return V3(x * scalar, y * scalar, z * scalar);
+}
+
+V3 operator*(f32 scalar, const V3& v3)
+{
+	return V3(v3.x * scalar, v3.y * scalar, v3.z * scalar);
+}
